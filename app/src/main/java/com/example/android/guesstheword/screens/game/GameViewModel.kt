7f7b1888c58a61d6976712107a941b2481eed38f
frankmorara <franklinenyabuto11@gmail.com>
2021-05-13 17:32:10 +0300

    Implementing the observer pattern using viewmodel on word and score values

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,18 +1,21 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
-    var word = ""
-    var score = 0
+    var word = MutableLiveData<String>()
+    var score = MutableLiveData<Int>()
     private lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel","GameViewModel Created")
         resetList()
         nextWord()
+        score.value = 0
+        word.value = ""
     }
 
     override fun onCleared() {
@@ -56,17 +59,17 @@ class GameViewModel: ViewModel() {
         if (wordList.isEmpty()) {
 //            gameFinished()
         } else {
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
 
     }
     fun onSkip() {
-        score--
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = (score.value)?.plus(1)
         nextWord()
     }
 
