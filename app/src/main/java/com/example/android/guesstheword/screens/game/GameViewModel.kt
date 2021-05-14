1aebf095627ce82f54b90c62966bffa1ea317c0d
frankmorara <franklinenyabuto11@gmail.com>
2021-05-14 10:39:30 +0300

    using a backing property to implement Encapsulation on ViewModel LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,21 +1,27 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
-    var word = MutableLiveData<String>()
-    var score = MutableLiveData<Int>()
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+    get() = _word
+
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+    get() = _score
     private lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel","GameViewModel Created")
         resetList()
         nextWord()
-        score.value = 0
-        word.value = ""
+        _score.value = 0
+        _word.value = ""
     }
 
     override fun onCleared() {
@@ -59,17 +65,17 @@ class GameViewModel: ViewModel() {
         if (wordList.isEmpty()) {
 //            gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+       _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
